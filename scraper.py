eed0d6c61a14697a9aab01b54a60435fe66255c9
ANAND KUMAR PANDEY <ap9012928@gmail.com>
2026-03-03 19:51:21 +0530

    Refactor scraper to use Selenium for web scraping

--- scraper.py
+++ scraper.py
@@ -1,50 +1,61 @@
 import sys
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Creating Function that takes url from command line and extracts title, body text and outlinks for that url
+# Creating Function that takes url from command line and extracts title, body text and outlinks from that page using selenium 
 def get_page_data(url):
-    response = requests.get(url, headers={"User-Agent": "StudentProject/1.0"})
-    if response.status_code != 200:
-        print("Failed to fetch:", url)
-        sys.exit(1)
-
-    soup = BeautifulSoup(response.text, "html.parser")
     
-    if soup.title:
-        title = soup.title.get_text(strip=True)
+    # Starting chrome browser
+    driver = webdriver.Chrome()
+    
+    # Opening the given url
+    driver.get(url)
+
+    # Getting title of page
+    if driver.title:
+        title = driver.title
     else:
         title = "No title"
 
-    for s in soup.find_all("script"):
-        s.decompose()
-
-    if soup.body:
-        body_text = soup.body.get_text(" ", strip=True)
+    # Getting body text safely
+    elements = driver.find_elements(By.TAG_NAME, "body")
+    if elements:
+        body_text = elements[0].text
     else:
         body_text = ""
 
+    # Collecting all links from page
     links = []
-    for a in soup.find_all("a"):
-        href = a.get("href")
+    link_elements = driver.find_elements(By.TAG_NAME, "a")
+    for e in link_elements:
+        href = e.get_attribute("href")
         if href:
             links.append(href)
 
-    return title, body_text, links
+    # Closing browser after work is done
+    driver.quit()
 
-# Checking if not valid command line argument 
+    return title, body_text, links     # Returning title, body_text and outlinks
+
+
+# Checking correct number of command line arguments
 if len(sys.argv) != 2:
-    print("Usage: python scraper.py url")
+    print("Command line arguments should be like: python scraper.py url")
     sys.exit(1)
 
-url = sys.argv[1]       # Taking url from command line argument
-title, body_text, links = get_page_data(url)       # Extracting Page Title, Page Body Text and Page Outlinks
-print()
-print("TITLE: ", title)       # Prints Title
-print()
-print("BODY_TEXT: ", body_text)        # Prints Body Text
-print()
-print("OUTLINKS:")             
-for link in links:          
-    print(link)            # Prints Outlinks (one per line)
+url = sys.argv[1]
 
+# Calling function to fetch page data
+title, body_text, links = get_page_data(url)
+
+print()          # For better readability in output
+print("TITLE:", title)
+print()    
+print("BODY_TEXT:", body_text)
+print()
+print("OUTLINKS:")
+
+# Printing links one by one
+for link in links:
+    print(link)
+print()
